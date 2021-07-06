bd8c3662f67dbd710b23c27924d0c8c49df237a2
张丽彩 <51307815+zlccly@users.noreply.github.com>
2021-07-07 01:36:59 +0800

    Add files via upload

---
+++ test.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <ImageLoader 
+	src="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?fit=crop&crop=entropy&w=3456&h=2304"
+  placeholder="https://images.pexels.com/photos/33109/fall-autumn-red-season.jpg?w=100"
+/>
+  </div>
+</template>
+
+<script>
+import ImageLoader from "./"
+  export default {
+    components:{
+      ImageLoader
+    },
+  
+  }
+</script>
