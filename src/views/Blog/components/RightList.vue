c9ea8348eb7edaaf0f95c99672150d5c2aa6e368
Lucky <1360495823@qq.com>
2021-08-08 21:25:08 +0800

    RightList组件

---
+++ src/views/Blog/components/RightList.vue
@@ -0,0 +1,45 @@
+<template>
+  <ul class="right-list-container">
+    <li v-for="(item, i) in list" :key="i" >
+      <span :class="{ active: item.isSelected }" @click="handleClick(item)"> {{ item.name }}</span>
+      <RightList :list="item.children" @select="handleClick"/>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  name: "RightList",
+  data() {
+    return {};
+  },
+  props: {
+    list: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  methods:{
+    handleClick(item){
+      this.$emit('select',item);
+    }
+  }
+};
+</script>
+<style scoped lang='less'>
+@import "~@/styles/var.less";
+.right-list-container {
+  list-style: none;
+  padding: 0;
+  .right-list-container {
+    margin-left: 1em;
+  }
+  li {
+    line-height: 40px;
+    min-height: 40px;
+    cursor: pointer;
+    .active {
+      color: @warn;
+    }
+  }
+}
+</style>
---
+++ src/views/Blog/components/test.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+      <RightList :list="list" @select="handleSelect"/>
+  </div>
+</template>
+<script>
+import RightList from "./RightList.vue";
+export default {
+  components: {
+    RightList,
+  },
+  data() {
+    return {
+      list: [
+        { name: "a", isSelected: false },
+        { name: "b", isSelected: false },
+        {
+          name: "c",
+          isSelected: true,
+          children: [
+            {
+              name: "c-1",
+              isSelected: false,
+              children: [
+                { name: "c-1-1", isSelected: false },
+                { name: "c-1-2", isSelected: false },
+                { name: "c-1-3", isSelected: false },
+              ],
+            },
+            { name: "c-2", isSelected: false },
+            { name: "c-3", isSelected: false },
+            { name: "c-4", isSelected: false },
+          ],
+        },
+        { name: "d", isSelected: false },
+      ],
+    };
+  },
+  methods:{
+      handleSelect(item){
+          console.log(item)
+      }
+  }
+};
+</script>
+<style scoped lang='less'>
+</style>
