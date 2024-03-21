a47a40cfdf028155b55f8c53ed6b756d54bcdaf4
aha <1360495823@qq.com>
2024-03-21 16:39:21 +0800

    分页组件07

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,29 @@
 <template>
   <div id="app">
-    <Icon type="home" />
+    <Pager :total="total" :current="current" @pageChange="handlePageChange($event)" />
   </div>
 </template>
 
 <script>
-import Icon from "./components/Icon.vue"
+import Pager from "./components/Pager.vue"
 
 export default {
   name: "App",
   data() {
     return {
+      current: 1,
+      total: 302,
     }
   },
   components: {
-    Icon
-  }
+    Pager
+  },
+  methods: {
+    handlePageChange(newPage) {
+      this.current = newPage;
+      console.log("加载当前页数据");
+    },
+  },
 
 }
 </script>
