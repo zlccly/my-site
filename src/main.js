b04f4f4018cd74d83588e13e6e84e042bcb8f428
aha <1360495823@qq.com>
2024-05-06 13:03:47 +0800

    格式化

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from "vue";
+import App from "./App.vue";
 import "./styles/global.less";
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App),
+}).$mount("#app");
